f8f9f98e166bc9320dfe2bce78a488dcd77a355a
Leeraberry-code <leeramasenya@gmail.com>
2024-10-12 01:10:33 +0200

    Add files via upload

---
+++ routes/learnerRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const learnerController = require('../controllers/learnerController');
+const router = express.Router();
+
+// Define routes for admin operations
+router.get('/learners', learnerController.getAllLearners);
+router.get('/learners/:id', learnerController.getLearnerById);
+router.post('/learners', learnerController.createLearner);
+router.put('/learners/:id', learnerController.updateLearner);
+router.delete('/learners/:id', learnerController.deleteLearner);
+
+module.exports = router;
