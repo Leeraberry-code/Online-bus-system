2b3bdd373789f72b45fc81c0bfca24f6b1fe16e5
Leeraberry-code <leeramasenya@gmail.com>
2024-10-12 03:04:40 +0200

    added server side logic for the bus

---
+++ server/controllers/busController.js
@@ -0,0 +1,46 @@
+const Bus = require('../models/bus');
+
+// Get all buses
+exports.getAllBuses = (req, res) => {
+    Bus.getAllBuses((err, buses) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(buses);
+    });
+};
+
+// Get bus by ID
+exports.getBusById = (req, res) => {
+    const { id } = req.params;
+    Bus.getBusById(id, (err, bus) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(bus);
+    });
+};
+
+// Create new bus
+exports.createBus = (req, res) => {
+    const busData = req.body;
+    Bus.createBus(busData, (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json({ message: 'Bus created successfully', id: result.insertId });
+    });
+};
+
+// Update existing bus
+exports.updateBus = (req, res) => {
+    const { id } = req.params;
+    const busData = req.body;
+    Bus.updateBus(id, busData, (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json({ message: 'Bus updated successfully' });
+    });
+};
+
+// Delete bus by ID
+exports.deleteBus = (req, res) => {
+    const { id } = req.params;
+    Bus.deleteBus(id, (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json({ message: 'Bus deleted successfully' });
+    });
+};
