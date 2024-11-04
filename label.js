df3c4dd872c83ba4ad2ea482098383dfe0ad4a90
Leeraberry-code <leeramasenya@gmail.com>
2024-11-04 13:19:27 +0200

    Delete label.js

--- label.js
+++
@@ -1,9 +0,0 @@
-import { LabelHTMLAttributes } from "react";
-
-interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {}
-
-export const Label = ({ className, ...props }: LabelProps) => {
-  return (
-    <label className={`block text-sm font-medium text-gray-700 ${className}`} {...props} />
-  );
-};
